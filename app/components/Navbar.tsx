c0b070e5760ec2cea7d3821aca83a19af11dcd92
adambenaceur <benaceur.adam@gmail.com>
2023-05-24 16:58:34 -0600

    added nav bar with updated navigation menus as well as a search icon and bag icon

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,18 +1,54 @@
 "use client"
 
-import Container from "./container"
+
+import { BsBag, BsSearch } from 'react-icons/bs';
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbar-container">
-        <Container>
+        <div className="container" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+          <div className='container-items' style={{ display: 'flex', alignItems: 'center' }}>
+            <h1 className="container-logo" style={{ fontSize: '1.5rem' }}>
+              timelineleap
+            </h1>
 
-        </Container>
+            <nav style={{ marginRight: '10px' }}>
+              <ul style={{ display: 'flex', listStyle: 'none', padding: 0 }}>
+                <li style={{ marginRight: '10px', marginLeft: '10px' }}>
+                  <a href="/">
+                    <span>
+                      Home
+                    </span>
+                  </a>
+                </li>
+                <li style={{ marginRight: '10px' }}>
+                  <a href="/collections/all">
+                    <span>
+                      Catalog
+                    </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/pages/contact">
+                    <span>
+                      Contact
+                    </span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="react-icons" style={{ display: 'flex', alignItems: 'center' }}>
+            <BsSearch size={18} style={{ marginRight: '10px', marginLeft: '10px' }} />
+            <BsBag size={18} style={{ marginRight: '10px', marginLeft: '10px' }} />
+          </div>
+        </div>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
+
