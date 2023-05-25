9dae0a162e2f971dadfb0bb2f722d101cecfeeba
adambenaceur <benaceur.adam@gmail.com>
2023-05-24 18:17:42 -0600

    added usePathname for underlining the current nav item based on the pathname

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,36 +1,39 @@
 "use client"
 
-
 import { BsBag, BsSearch } from 'react-icons/bs';
+import { usePathname } from 'next/navigation'
+
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
     <div className="navbar">
       <div className="navbar-container">
         <div className="container" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
           <div className='container-items' style={{ display: 'flex', alignItems: 'center' }}>
-            <h1 className="container-logo" style={{ fontSize: '1.5rem' }}>
+            <h1 className="container-logo" style={{ fontSize: '1.5rem', fontWeight: '300' }}>
               timelineleap
             </h1>
 
             <nav style={{ marginRight: '10px' }}>
-              <ul style={{ display: 'flex', listStyle: 'none', padding: 0 }}>
-                <li style={{ marginRight: '10px', marginLeft: '10px' }}>
-                  <a href="/">
+              <ul className="nav-items" style={{ display: 'flex', listStyle: 'none', padding: 0 }}>
+                <li className="nav-item" style={{ marginRight: '10px', marginLeft: '10px' }}>
+                  <a href="/" style={pathname === '/' ? { textDecoration: 'underline' } : {}}>
                     <span>
                       Home
                     </span>
                   </a>
                 </li>
-                <li style={{ marginRight: '10px' }}>
-                  <a href="/collections/all">
+                <li className="nav-item" style={{ marginRight: '10px' }}>
+                  <a href="/collections/all" style={pathname === '/collections/all' ? { textDecoration: 'underline' } : {}}>
                     <span>
                       Catalog
                     </span>
                   </a>
                 </li>
-                <li>
-                  <a href="/pages/contact">
+                <li className="nav-item">
+                  <a href="/pages/contact" style={pathname === '/pages/contact' ? { textDecoration: 'underline' } : {}}>
                     <span>
                       Contact
                     </span>
@@ -51,4 +54,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
