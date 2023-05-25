6c31e46e00c2d60f4164f6f32058f76a58d89cbb
adambenaceur <benaceur.adam@gmail.com>
2023-05-24 20:09:58 -0600

    added collections with improved ui

--- app/components/Collection.tsx
+++ app/components/Collection.tsx
@@ -1,29 +1,33 @@
 "use client"
 
-import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation';
 
 const Collection = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    // sample product data
-    const products = [
-        { id: 1, name: 'Product 1', price: '100', image: 'product1.jpg' },
-        { id: 2, name: 'Product 2', price: '200', image: 'product2.jpg' },
-        { id: 3, name: 'Product 3', price: '300', image: 'product3.jpg' },
-        // add more products as needed...
-    ];
+  const items = [
+    { id: 1, name: 'Journals', image: 'journals.jpg' },
+    { id: 2, name: 'Watches', image: 'watches.jpg' },
+    { id: 3, name: 'Calendars', image: 'calendars.jpg' },
+    { id: 4, name: 'Pens', image: 'pens.jpg' },
+  ];
 
-    return (
-        <div className="collection">
-            {products.map(product => (
-                <div key={product.id} className="product-card" onClick={() => router.push(`/product/${product.id}`)}>
-                    <img src={product.image} alt={product.name} className="product-image" />
-                    <h3 className="product-name">{product.name}</h3>
-                    <p className="product-price">${product.price}</p>
-                </div>
-            ))}
-        </div>
-    )
-}
+  return (
+    <div className="collection">
+      <h2 className="collection-title">Collection</h2>
+      <div className="collection-grid">
+        {items.map(item => (
+          <div key={item.id} className="item-card">
+            <img src={item.image} alt={item.name} className="item-image" />
+            <h3 className="item-name">{item.name}</h3>
+            <button className="item-button" onClick={() => router.push(`/collection/${item.id}`)}>
+              View More
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Collection;
