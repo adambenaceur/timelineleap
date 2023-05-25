e533fb4d0f99e652c060548b8c353e190ab53853
adambenaceur <benaceur.adam@gmail.com>
2023-05-24 18:46:01 -0600

    added a feature where the current pathname is underlined in the navbar and hovering allows for underlining new nav links

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,49 +3,40 @@
 import { BsBag, BsSearch } from 'react-icons/bs';
 import { usePathname } from 'next/navigation'
 
-
 const Navbar = () => {
   const pathname = usePathname();
 
   return (
     <div className="navbar">
       <div className="navbar-container">
-        <div className="container" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-          <div className='container-items' style={{ display: 'flex', alignItems: 'center' }}>
-            <h1 className="container-logo" style={{ fontSize: '1.5rem', fontWeight: '300' }}>
-              timelineleap
-            </h1>
+        <div className="container">
+          <div className='container-items'>
+            <h1 className="container-logo">timelineleap</h1>
 
-            <nav style={{ marginRight: '10px' }}>
-              <ul className="nav-items" style={{ display: 'flex', listStyle: 'none', padding: 0 }}>
-                <li className="nav-item" style={{ marginRight: '10px', marginLeft: '10px' }}>
-                  <a href="/" style={pathname === '/' ? { textDecoration: 'underline' } : {}}>
-                    <span>
-                      Home
-                    </span>
-                  </a>
-                </li>
-                <li className="nav-item" style={{ marginRight: '10px' }}>
-                  <a href="/collections/all" style={pathname === '/collections/all' ? { textDecoration: 'underline' } : {}}>
-                    <span>
-                      Catalog
-                    </span>
+            <nav>
+              <ul className="nav-items">
+                <li className="nav-item">
+                  <a href="/" className={pathname === '/' ? "active" : ""}>
+                    <span>Home</span>
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a href="/pages/contact" style={pathname === '/pages/contact' ? { textDecoration: 'underline' } : {}}>
-                    <span>
-                      Contact
-                    </span>
+                  <a href="/collections/all" className={pathname === '/collections/all' ? "active" : ""}>
+                    <span>Catalog</span>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="/pages/contact" className={pathname === '/pages/contact' ? "active" : ""}>
+                    <span>Contact</span>
                   </a>
                 </li>
               </ul>
             </nav>
           </div>
 
-          <div className="react-icons" style={{ display: 'flex', alignItems: 'center' }}>
-            <BsSearch size={18} style={{ marginRight: '10px', marginLeft: '10px' }} />
-            <BsBag size={18} style={{ marginRight: '10px', marginLeft: '10px' }} />
+          <div className="react-icons">
+            <BsSearch size={18} />
+            <BsBag size={18} />
           </div>
         </div>
       </div>
